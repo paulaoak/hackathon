fda01c63b33e0a150a3d139f81551fcb8e67cda3
Paula Cordero Encinar <pc2222@ic.ac.uk>
2024-05-25 15:09:37 +0100

    update

--- main.py
+++ main.py
@@ -12,10 +12,6 @@ templates = Jinja2Templates(directory="templates")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/get_data")
-async def get_data(input1: str = Form(...), input2: str = Form(...)):
-    result = print("This paper will help you")
-    return {"result": result}
     
 @app.post("/sum")
 async def calculate_sum(input1: str = Form(...), input2: str = Form(...)):
