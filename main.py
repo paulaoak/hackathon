b407de695174e5bf950209e468ba6686048b627f
Paula Cordero Encinar <pc2222@ic.ac.uk>
2024-05-25 14:59:21 +0100

    update

--- main.py
+++ main.py
@@ -18,4 +18,7 @@ async def get_data(input1: str = Form(...), input2: str = Form(...)):
     result = print("This paper will help you")
     return {"result": result}
     
-
+@app.post("/sum")
+async def calculate_sum(input1: str = Form(...), input2: str = Form(...)):
+    result = print("This paper will help you")
+    return {"sum": result}
