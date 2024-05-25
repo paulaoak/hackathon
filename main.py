0d8f886ffd85e612b78ffe4785828c21ac62bb76
Paula Cordero Encinar <pc2222@ic.ac.uk>
2024-05-25 14:46:01 +0100

    update

--- main.py
+++ main.py
@@ -14,10 +14,8 @@ async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/get_data")
-async def get_data(request: Request, input1: str = Form(...), input2: str = Form(...)):
-    async with aiohttp.ClientSession() as session:
-        api_url = 'http://127.0.0.1:8001/'
-        params = {'param1': input1, 'param2': input2}
-        result = print("This paper will help you")
-        return {"data": result}
+async def get_data(input1: str = Form(...), input2: str = Form(...)):
+    result = print("This paper will help you")
+    return {"result": result}
+    
 
